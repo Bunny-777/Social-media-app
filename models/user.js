2cdb8a2351e9359eccd4b37bf7cebc2354c50e81
Bunny <bunny202410@gmail.com>
2025-02-05 23:59:53 +0530

    added mongodb uri

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost/socialapp');
+
+// mongoose.connect('mongodb://localhost/socialapp');
+mongoose.connect('mongodb+srv://bunny202410:lemon@cluster0.8xiin.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
 const userSchema=mongoose.Schema({
     username:String,
